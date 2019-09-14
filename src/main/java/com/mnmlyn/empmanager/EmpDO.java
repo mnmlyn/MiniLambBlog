79fe22debdadd89c6d7ab60483c398d2886b9633
mnmlyn <mnmlyn@163.com>
2019-09-14 20:30:28 +0800

    将int修改为Integer，在DO类中，应该使用包装类型

--- src/main/java/com/mnmlyn/empmanager/EmpDO.java
+++ src/main/java/com/mnmlyn/empmanager/EmpDO.java
@@ -3,12 +3,17 @@ package com.mnmlyn.empmanager;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 员工信息的数据对象
+ *
+ * @author mnmlyn
+ */
 @Getter
 @Setter
 public class EmpDO {
-    private int id;
+    private Integer id;
     private String name;
-    private int age;
+    private Integer age;
 
     @Override
     public String toString() {
